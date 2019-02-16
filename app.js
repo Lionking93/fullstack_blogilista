3255dabc3c23e7319403cdc9f5a239a34b90b625
Lionking93 <leo.kallonen@gmail.com>
2019-02-16 13:32:22 +0200

    osa 4 t4.20

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
 const usersRouter = require('./controllers/users')
@@ -15,6 +16,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use(middleware.tokenExtractor)
 app.use('/api', usersRouter)
 app.use('/api', blogsRouter)
 app.use('/api', loginRouter)

f867e30fe05f6951a639ccae6540100575a90cf5
Lionking93 <leo.kallonen@gmail.com>
2019-02-16 13:08:53 +0200

    osa 4 t4.18

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -16,5 +17,6 @@ app.use(bodyParser.json())
 
 app.use('/api', usersRouter)
 app.use('/api', blogsRouter)
+app.use('/api', loginRouter)
 
 module.exports = app

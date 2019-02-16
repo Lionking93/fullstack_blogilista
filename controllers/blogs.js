4adddc049c9579a157d846f2c235a015102c8c91
Lionking93 <leo.kallonen@gmail.com>
2019-02-16 13:22:11 +0200

    osa 4 t4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog.js')
 const User = require('../models/user.js')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/blogs', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -10,15 +19,22 @@ blogsRouter.get('/blogs', async (request, response) => {
 blogsRouter.post('/blogs', async (request, response, next) => {
   const newBlog = request.body
 
-  if (newBlog.likes === undefined)
-    newBlog.likes = 0
-
-  const user = await User.findOne({})
-  newBlog.user = user.id
-
-  const blogToBeSaved = new Blog(newBlog)
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    if (newBlog.likes === undefined)
+      newBlog.likes = 0
+
+    const user = await User.findById(decodedToken.id)
+    newBlog.user = user.id
+
+    const blogToBeSaved = new Blog(newBlog)
+
     const result = await blogToBeSaved.save()
     user.blogs = user.blogs.concat(result.id)
     await user.save()
@@ -51,6 +67,10 @@ blogsRouter.put('/blogs/:id', async (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   next(error)

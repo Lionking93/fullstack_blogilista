1a71e98a66e873967dbb17f26941c0fb58ee00ce
Lionking93 <leo.kallonen@gmail.com>
2019-02-16 13:53:28 +0200

    osa 4 t4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -36,7 +36,18 @@ blogsRouter.post('/blogs', async (request, response, next) => {
 
 blogsRouter.delete('/blogs/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() !== decodedToken.id.toString()) {
+      return response.status(401).json({ error: 'invalid user' })
+    }
+
+    await blog.remove()
     response.status(204).end()
   } catch(error) {
     next(error)
